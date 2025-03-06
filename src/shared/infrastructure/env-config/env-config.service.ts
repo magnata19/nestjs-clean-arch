0d4ad43ca47f1d547ef9d9ac5b836077351aab9e
Davidson <dehpacifico@gmail.com>
2025-03-05 23:12:18 -0300

    criando modulo de configuracao

---
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { EnvConfigInterface } from './env-config.interface';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class EnvConfigService implements EnvConfigInterface {
+
+  constructor(private configService: ConfigService) { }
+
+  getAppPort(): number {
+    return Number(this.configService.get<number>('PORT'));
+  };
+  getNodeEnv(): string {
+    return this.configService.get<string>('NODE_ENV') ?? '';
+  };
+}
