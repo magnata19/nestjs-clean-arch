d66fd644120c990a2e59e90e2a0a47c56940922c
Davidson <dehpacifico@gmail.com>
2025-04-09 21:39:07 -0300

    criando novos testes

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,28 @@
-export class User {}
+export type UserProps = {
+  name: string;
+  email: string;
+  password: string;
+  createdAt?: Date;
+}
+
+export class UserEntity {
+  constructor(public readonly props: UserProps) {
+    this.props.createdAt = this.props.createdAt ?? new Date();
+  }
+
+  getName() {
+    return this.props.name;
+  }
+
+  get email() {
+    return this.props.email;
+  }
+
+  get password() {
+    return this.props.password;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+}
