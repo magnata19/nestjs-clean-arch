7b7443d9286805e01c4dff66dc1587480acffab3
Davidson <dehpacifico@gmail.com>
2025-04-12 22:34:03 -0300

    criando testes unitarios para ClassValidatorFields

--- src/shared/domain/validators/class-validator-fields.ts
+++ src/shared/domain/validators/class-validator-fields.ts
@@ -9,11 +9,11 @@ export abstract class ClassValidatorFields<PropsValidated> implements ValidatorF
     if (errors.length) {
       this.errors = {}
       for (const error of errors) {
-        const field = error.property;
+        const field = error.property
         this.errors[field] = Object.values(error.constraints)
       }
     } else {
-      this.validatedData = data
+      this.validatedData = data;
     }
     return !errors.length;
   }
