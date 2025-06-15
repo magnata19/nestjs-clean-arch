73645aaeda0999d7aa0dfaa3950fe6246fe9c141
Davidson <dehpacifico@gmail.com>
2025-06-15 16:45:59 -0300

    adding SearchResult class

--- src/shared/domain/repositories/searchable-repository-contract.ts
+++ src/shared/domain/repositories/searchable-repository-contract.ts
@@ -11,6 +11,16 @@ export type SearchProps<Filter = string> = {
   filter?: Filter | null; // Filtro para pesquisa, pode ser um termo de busca ou outro critério
 };
 
+export type SearchResultProps<E extends Entity, Filter> = {
+  items: E[];
+  total: number;
+  currentPage: number;
+  perPage: number;
+  sort: string | null;
+  sortDir: string | null;
+  filter: Filter | null;
+}
+
 export class SearchParams {
   protected _page?: number;
   protected _perPage = 15;
@@ -82,9 +92,44 @@ export class SearchParams {
   }
 }
 
+export class SearchResult<E extends Entity, Filter = string> {
+  readonly items: E[];
+  readonly total: number;
+  readonly currentPage: number;
+  readonly perPage: number;
+  readonly lastPage: number;
+  readonly sort: string | null;
+  readonly sortDir: string | null;
+  readonly filter: Filter | null;
+
+  constructor(props: SearchResultProps<E, Filter>) {
+    this.items = props.items;
+    this.total = props.total;
+    this.currentPage = props.currentPage;
+    this.perPage = props.perPage;
+    this.lastPage = Math.ceil(this.total / this.perPage); // 15 items divididos por 3 perPage = 5 que é a ultima página
+    this.sort = props.sort ?? null;
+    this.sortDir = props.sortDir ?? null;
+    this.filter = props.filter ?? null;
+  }
+
+  toJson(forceEntity: boolean = false) {
+    return {
+      items: forceEntity ? this.items.map(item => item.toJSON()) : this.items,
+      total: this.total,
+      currentPage: this.currentPage,
+      perPage: this.perPage,
+      lasPage: this.lastPage,
+      sort: this.sort,
+      sortDir: this.sortDir,
+      filter: this.filter
+    }
+  }
+}
+
 export interface SearchableRepositoryInterface<E extends Entity,
   SearchInput, // tipo de entrada para a pesquisa ex: nome, email, etc
   SearchOutput // tipo de saida da pesquisa ex: numero de resultados, lista de entidades, etc
 > extends RepositoryInterface<E> {
-  search(input: SearchParams): Promise<SearchOutput>;
+  search(input: SearchParams): Promise<SearchResult<E, any>>;
 }
