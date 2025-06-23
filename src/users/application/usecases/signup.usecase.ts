93b901628bdd5162e7b0d2b4177679ca4ea967e8
Davidson <dehpacifico@gmail.com>
2025-06-23 19:02:00 -0300

    adding update-user-usecase

--- src/users/application/usecases/signup.usecase.ts
+++ src/users/application/usecases/signup.usecase.ts
@@ -2,7 +2,7 @@ import { BadRequestError } from "../../../shared/application/errors/bad-request-
 import { UserRepository } from "@/users/domain/repositories/user.repository";
 import { UserEntity } from "@/users/domain/entities/user.entity";
 import { HashProvider } from "@/shared/application/providers/hash-provider";
-import { UserOutput } from "../dtos/user-output";
+import { UserOutput, UserOutputMapper } from "../dtos/user-output";
 import { UseCase as DefaultUseCase } from "@/shared/application/usecases/use-case";
 
 export namespace SignUpUsecase {
@@ -34,7 +34,7 @@ export namespace SignUpUsecase {
 
       await this.userRepository.insert(entity);
 
-      return entity.toJSON() as Output;
+      return UserOutputMapper.toOutput(entity);
 
     }
   }
